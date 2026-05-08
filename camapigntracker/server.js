f928011fa3006e8aaa5f9431c4b1576694cff9f9
gokulsingh2 <gokul.singh29654@gmail.com>
2026-05-08 13:00:20 +0530

    fix: serve static frontend files

--- camapigntracker/server.js
+++ camapigntracker/server.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use(express.static(path.join(__dirname, '../')));
 
 app.use("/auth", require("./auth"));
 app.use("/campaign", require("./campaign"));
